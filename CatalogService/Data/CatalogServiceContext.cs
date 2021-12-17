e7ca6e39da975ee2e1ac7a4a201091eba45074aa
Jens Göst <jens.gost@gmail.com>
2021-12-17 12:10:03 +0100

    added new project: catalogService

---
+++ CatalogService/Data/CatalogServiceContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CatalogService.Models.Product;
+
+namespace CatalogService.Data
+{
+    public class CatalogServiceContext : DbContext
+    {
+        public DbSet<CatalogProduct> Products { get; set; }
+        public CatalogServiceContext(DbContextOptions<CatalogServiceContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ CatalogService/Models/Product/CatalogProduct.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogService.Models.Product
+{
+    public class CatalogProduct
+    {
+        public CatalogProduct(int id, string name, string description, string imageUrl, double price, string articleNumber, string urlSlug)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+            ArticleNumber = articleNumber;  
+            UrlSlug = urlSlug;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public string ArticleNumber { get; set; }
+        public string UrlSlug { get; set; }
+    }
+}
